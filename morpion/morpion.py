134fcd2dd8e8e2da047b40ab14cf280b84039830
Marc Augier <m.augier@me.com>
2017-06-26 14:15:38 +0200

    Initialisation

---
+++ morpion/morpion.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python
+
+import sys
+"""
+Petit jeu de morpion
+-----------------------
+Une grille 3x3 et chaque joueur à tour de rôle va pose un pion,
+représenté par un O ou un X.
+Le premier qui aligne 3 pions a gagné !
+
+(c) Marc Augier 2017
+    m.augier@me.com
+
+"""
+
+print("Petit jeu de morpion")
+
+"""
+
+ ici le code
+
+"""
+
+print("C'est terminé")
