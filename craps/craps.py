aa6ed4988f2fb576f6a29886351bb3d5f2aa7ca7
Marc Augier <m.augier@me.com>
2020-11-16 23:18:24 +0100

    version du cours

--- craps/craps.py
+++ craps/craps.py
@@ -1,114 +1,117 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
+
 """
-Craps
+CRAPS
 -----
 
-Le Craps est un jeu de dés populaire en Amérique du Nord
+Jeu de craps
 
-L'ordinateur lance les dés, si au premier lancé vous obtenez 7 ou 11, vous doublez votre mise. Si vous obtenez 2, 3 ou 12, vous perdez.
-Pour toute autre valeur, vous relancez les dés, jusqu'à obtenir la même valeur de nouveau, ce qui doublera votre mise.
-Mais si dans l'intervalle vous obtenez un 7, vous perdez.
-
-(c) Marc Augier 2019
-    m.augier@me.com
 """
 from random import randint
 
 class craps:
-        def __init__(self, p):
 
-            print("===============")
-            print("=  C R A P S  =")
-            print("===============")
+    def __init__(self, p):
+        self.portefeuille = p
+        self.jouer = True
+        self.mise = 0
+        self.dé1 = 0
+        self.dé2 = 0
+        self.tour = 1
+        self.cible = 0
 
-            self.portefeuille = p
-            self.mise = 0
-            self.tour = 1
-            self.cible = 0
-            self.joue = True
+        print("+-------------+")
+        print("+  C R A P S  +")
+        print("+-------------+\n")
 
-        def affiche_regles(self):
-            print("""Le Craps est un jeu de dés populaire en Amérique du Nord
+        print("L'ordinateur lance les dés.\nSi au premier lancé vous obtenez 7 ou 11, vous doublez votre mise.")
+        print("Si vous obtenez 2, 3 ou 12 vous perdez")
+        print("Pour toute aure valeur vous relancez les dés, jusqu'à obtenir la même valeur de nouveau, ce qui doublera votre mise")
+        print("Si dans l'intervalle vous obtenez un 7, vous perdez.\n")
 
-            L'ordinateur lance les dés, si au premier lancé vous obtenez 7 ou 11, vous doublez votre mise. Si vous obtenez 2, 3 ou 12, vous perdez.
-            Pour toute autre valeur, vous relancez les dés, jusqu'à obtenir la même valeur de nouveau, ce qui doublera votre mise.
-            Mais si dans l'intervalle vous obtenez un 7, vous perdez.
-            """)
+    def en_cours(self):
+        return self.jouer
 
-        def getJoue(self):
-            return self.joue
+    def miser(self):
+        mise = 0
         
-        def jeuGagne(self):
-            print("Vous avez gagné {} ".format(2*self.mise))
-            self.portefeuille += 2*self.mise
-            self.fin()      
-
-        def jeuPerdu(self):
-            print("Vous avez perdu !")      
-            self.fin()
-
-        def debut(self):
-            self.tour = 1
-            self.cible = 0
-            self.joue = True
-
-        def fin(self):
-            self.joue = False
-
-        def getCible(self):
-            return self.cible
+        print("Vous avez {} en portefeuille.".format(self.portefeuille))
+        if self.tour >1:
+            print("\tVous devez réaliser {} pour gagner".format(self.cible))
         
-        def setCible(self, c):
-            self.cible = c
+        while(mise<=0 or mise > self.portefeuille):
+            try:
+                mise = int(input("Combien vous voulez miser ? => "))
+            except ValueError:
+                print("\n\t\tMerci de rentrer une valeur numérique\n")
+                mise = 0
+        self.mise = mise
+        self.portefeuille -= mise
 
-        def enCours(self):
-            if self.portefeuille > 0:
-                return True
+    def lancer(self):
+        self.dé1 = randint(1,6)
+        self.dé2 = randint(1,6)
+
+    def analyse(self):
+        valeur = self.dé1+self.dé2
+        print("Vous avez tiré {} et {}, ce qui fait {}".format(self.dé1, self.dé2, valeur))
+        if self.tour == 1:
+            if valeur == 7 or valeur == 11:
+                self.gagne()
+            elif valeur == 2 or valeur == 3 or valeur == 12:
+                self.perdu()
+            elif self.portefeuille == 0:
+                print("\nVous êtes ruiné")
+                self.perdu()
             else:
-                return False
+                self.tour += 1
+                self.cible = valeur
+        else:
+            if valeur == 7:
+                self.perdu()
+            elif valeur == self.cible:
+                self.gagne()
+            elif self.portefeuille == 0:
+                print("\nVous êtes ruiné")
+                self.perdu()
+    
+    def gagne(self):
+        print("Vous avez gagné")
+        self.portefeuille += 2*self.mise
+        print("Vous avez maintenant {} dans votre portefeuille".format(self.portefeuille))
+        self.jouer =False
 
-        def miser(self):
-            mise = 0
-            while(mise<=0 or mise > self.portefeuille):
-                mise=input("\nVous avez {} en poche. Combien vous voulez miser ? ==> ".format(self.portefeuille))
-                try:
-                    mise = int(mise)
-                except ValueError:
-                    print("\t\tMerci d'entrer une valeur numérique")
-                    mise = 0
-            self.mise = mise
-            self.portefeuille -= mise
+    def perdu(self):
+        print("Vous avez perdu")
+        print("Vous avez maintenant {} dans votre portefeuille".format(self.portefeuille))
+        self.jouer =False
 
-        def lancer(self):
-            return randint(1,6)
+    def relance(self):
+        self.jouer = True
+        self.cible = 0
+        self.tour  = 1
 
-        def analyse(self, v1, v2):
-            valeur = v1 + v2
-            print("\n---> Vous avez tiré : {} et {}, ce qui fait {}".format(v1, v2, valeur))
-            if self.tour == 1:
-                if valeur==7 or valeur==11:
-                    self.jeuGagne()
-                elif valeur==2 or valeur==3 or valeur==12:
-                    self.jeuPerdu()
-                else:
-                    self.tour += 1
-                    self.setCible(valeur)
-            else:
-                if valeur==7:
-                    self.jeuPerdu()
-                elif valeur == self.getCible():
-                    self.jeuGagne()
+jeu = craps(200)
 
-"""
-    ça commence ici
-"""
-jeu = craps(500)
+nouveau_tour = True
 
-jeu.affiche_regles()
+while nouveau_tour:
+    while jeu.en_cours():
+        jeu.miser()
+        jeu.lancer()
+        jeu.analyse()
+    
+    if jeu.portefeuille > 0:
+        rep = ""
+        while rep != "oui" and rep != "non":    
+            rep = input("Continuer ? (oui, non) ")
+        
+        if rep == "non":
+            nouveau_tour = False
+        else:
+            jeu.relance()
+    else:
+        nouveau_tour = False
 
-while jeu.enCours():
-    jeu.debut()
-    jeu.miser()
-    while jeu.getJoue():
-        jeu.analyse(jeu.lancer(), jeu.lancer())
+print("\n\tAu revoir...")
